d00fa27c5cee801e6ea97a7a8dae7bc53d3ec68a
Arya <aryams11042000@gmail.com>
2022-07-06 13:14:24 +0530

    Hello World Commit

---
+++ src/main/java/com/tgt/rysetii/learningresoursesapiarya/LearningresoursesapiaryaApplication.java
@@ -0,0 +1,16 @@
+package com.tgt.rysetii.learningresoursesapiarya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresoursesapiaryaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LearningresoursesapiaryaApplication.class, args);
+		System.out.println("Hello World");
+
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningresoursesapiarya/LearningresoursesapiaryaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresoursesapiarya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresoursesapiaryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
